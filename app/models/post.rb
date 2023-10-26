611881fefca376b0f661b5727355d91e84ae7f98
agneta1991 <pupienytea@gmail.com>
2023-10-26 12:06:56 +0300

    added six validations for user & posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,11 +4,21 @@ class Post < ApplicationRecord
   has_many :likes
   after_save :update_post_counter
 
+  validates :title, presence: true
+  validates :title, length: { maximum: 250 }
+  validate :validation_comments_counter
+
   def update_post_counter
     author.update(post_counter: author.posts.count)
   end
 
   def most_recent_comments(limit = 5)
     comments.order(created_at: :desc).limit(limit)
+  end
+
+  def validation_comments_counter
+    return unless comments_counter.present? && (!comments_counter.is_a?(Integer) || comments_counter <= 0)
+
+    errors.add(:comments_counter, 'It must be a number greater than or equal to zero')
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,18 @@ class User < ApplicationRecord
   has_many :comments
   has_many :likes
 
+  validates :name, presence: true
+  validate :validation_posts_counter
+
   def most_recent_post(limit = 3)
     posts.order(created_at: :desc).limit(limit)
+  end
+
+  private
+
+  def validation_posts_counter
+    return unless posts_counter.present? && (!posts_counter.is_a?(Integer) || posts_counter <= 0)
+
+    errors.add(:posts_counter, 'It must be a number greater than or equal to zero')
   end
 end
