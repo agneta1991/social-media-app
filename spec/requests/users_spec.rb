5bd661d0928036f2b0f78236d10d5de08c935194
agneta1991 <pupienytea@gmail.com>
2023-10-30 14:15:21 +0200

    Added tests for index, cleaned up the code

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,27 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  describe 'GET /show' do
-    it 'renders a successful response' do
-      valid_attributes = { name: 'Agneta', id: 1 }
+  let(:user) { User.create(name: 'Agneta', id: 1) }
 
-      user = User.create!(valid_attributes)
-      get user_url(user)
+  describe 'GET /index' do
+    before { get users_url }
+
+    it 'renders a successful response' do
       expect(response).to be_successful
     end
 
     it 'renders the correct template' do
-      valid_attributes = { name: 'Agneta', id: 1 }
-      user = User.create!(valid_attributes)
-      get user_url(user)
+      expect(response).to render_template('users/index')
+    end
+
+    it 'includes correct placeholder text in the response body' do
+      expect(response.body).to include('<h1>Here is a list of users</h1>')
+    end
+  end
+
+  describe 'GET /show' do
+    before { get user_url(user) }
+
+    it 'renders a successful response' do
+      expect(response).to be_successful
+    end
+
+    it 'renders the correct template' do
       expect(response).to render_template('users/show')
     end
 
     it 'includes correct placeholder text in the response body' do
-      valid_attributes = { name: 'Agneta', id: 1 }
-      user = User.create!(valid_attributes)
-      get user_url(user)
-      expect(response.body).to include('<h1>Agneta</h1>')
+      expect(response.body).to include("<h1>#{user.name}</h1>")
     end
   end
 end
