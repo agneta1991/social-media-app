fe9714c022ba232c45b00a6edb7ee327a1aa9174
agneta1991 <pupienytea@gmail.com>
2023-10-31 15:24:48 +0200

    Added logic for all pages to display correct info

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,10 @@
 class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
-    @posts = @user.posts
+    @posts = @user.posts.includes(:comments)
   end
 
   def show
-    @post = Post.find(params[:id])
-    # @user = User.find(@post.author_id)
+    @post = Post.includes(:comments).find(params[:id])
   end
 end
