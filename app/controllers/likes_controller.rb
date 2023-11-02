caf579f6d93ecdd174d669e5576cea4fade510bf
agneta1991 <pupienytea@gmail.com>
2023-11-02 18:14:17 +0200

    Added logic to add like

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,19 @@
+class LikesController < ApplicationController
+  def new
+    @post = Post.find(params[:id])
+    @like = Like.new
+  end
+
+  def create
+    @post = Post.find(params[:id])
+    # @like = Like.new(user_id: @user.id, post_id: @post.id)
+    @like = @post.likes.new
+    @like.author = current_user
+
+    if @like.save
+      redirect_to user_post_path(@post.author, @post)
+    else
+      render :new
+    end
+  end
+end
