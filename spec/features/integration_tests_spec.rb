0609d40b2d6fc0f010f64c8f7d0154d1fdbc1ad3
agneta1991 <pupienytea@gmail.com>
2023-11-06 16:17:55 +0200

    Added tests for user show page

--- spec/features/integration_tests_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.feature "IntegrationTests", type: :feature do
-  
-end
---
+++ spec/features/posts_spec.rb
@@ -0,0 +1,57 @@
+require 'rails_helper'
+
+RSpec.describe 'Post', type: :feature do
+  user = User.first
+  post = Post.first
+  it 'shows user profile picture' do
+    visit user_posts_path(user)
+    expect(page).to have_selector("img[src='#{user.photo}']")
+  end
+
+  it 'shows usr name' do
+    visit user_posts_path(user)
+    expect(page).to have_content(user.name)
+  end
+
+  it 'displays number of posts of the user' do
+    visit user_posts_path(user)
+    expect(page).to have_content("Number of posts: #{user.post_counter}")
+  end
+
+  it 'displays post title' do
+    visit user_posts_path(user)
+    expect(page).to have_content(post.title)
+  end
+
+  it 'displays post body' do
+    visit user_posts_path(user)
+    expect(page).to have_content("#{post.text[0, 127]}...") if post.text.length > 130
+  end
+
+  it 'displays the first comment' do
+    visit user_posts_path(user)
+    first_comment = post.most_recent_comments.first
+    expect(page).to have_content("#{first_comment.author.name}: #{first_comment.text}")
+  end
+
+  it 'displays number of comments of each post' do
+    visit user_posts_path(user)
+    expect(page).to have_content("Comments: #{post.comments_counter}")
+  end
+
+  it 'displays number of likes of each post' do
+    visit user_posts_path(user)
+    expect(page).to have_content("Likes: #{post.likes_counter}")
+  end
+
+  it 'has a button to display all posts' do
+    visit user_posts_path(user)
+    expect(page).to have_button('Pagination')
+  end
+
+  it 'redirects when clicked on a post title' do
+    visit user_posts_path(user)
+    click_link(post.title)
+    expect(page).to have_current_path(user_post_path(user, post))
+  end
+end
