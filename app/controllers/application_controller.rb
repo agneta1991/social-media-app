7fa78d8f748f69550c6222744a9137f97f3df17c
agneta1991 <pupienytea@gmail.com>
2023-11-07 16:12:51 +0200

    Revert "setting up devise"
    This reverts commit 1c95b7e63fb6a72b905b93213a6cb0c38db54093.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,6 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name])
+  def current_user
+    @current_user ||= User.first
   end
+  helper_method :current_user
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -72,7 +72,5 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   # Raise error when a before_action's only/except options reference missing actions
-  config.action_mailer.delivery_method = :letter_opener
   config.action_controller.raise_on_missing_callback_actions = true
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
