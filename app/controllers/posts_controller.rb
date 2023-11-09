29ff0b56d25d9eb568278c15e9e307548147dc76
agneta1991 <pupienytea@gmail.com>
2023-11-09 11:01:38 +0200

    Added endpoint to display all user posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,8 @@ class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
     @posts = @user.posts.includes(:comments)
+
+    render json: @posts, except: %i(created_at updated_at)
   end
 
   def show
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,6 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
-
-    respond_to do |format|
-      # format.html
-      format.xml  { render :xml => @users }
-      format.json { render :json => @users }
-    end
   end
 
   def show
